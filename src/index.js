df052a67bbb9e855c545a8e4ebfded8fbb6048fc
Phil Ledoit <74996868+pledoit@users.noreply.github.com>
2021-11-13 15:43:37 -0500

    Nuke repo again

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import App from './App';
 import ToDoItemList from './ToDoItemList';
 import './index.css';
-
-// Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
 
 const reactContainer = document.getElementById("react-container");
 
