09d4c8c90e9233922cfe85d9494e81d93cd72356
Igor <i.a.kheyfets@gmail.com>
2021-11-13 22:41:17 -0500

    termination/origination tests

--- calls/call.py
+++ calls/call.py
@@ -1,7 +1,7 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
-
+from bs4 import BeautifulSoup
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
@@ -11,10 +11,16 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
+with open('instructions.xml') as f:
+    instructions = f.read()
+
+print(instructions)
+
 call = client.calls.create(
-                        url='http://demo.twilio.com/docs/voice.xml',
-                        to='+18554208536',
-                        from_='+16504894546'
+                        # url='http://demo.twilio.com/docs/voice.xml',
+                        twiml = '<Response><Gather input="speech" timeout="3"></Gather><Say>Ahoy there!</Say></Response>',
+                        to='+16174172510', #16177510502
+                        from_='+18507792976'
                     )
 
 print(call.sid)
