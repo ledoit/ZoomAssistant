21368fef2f4a5d8e1383cb8719168a957d1e5cf3
Igor Kheyfets <i.a.kheyfets@gmail.com>
2021-11-14 03:23:13 -0500

    zoom connection successful

--- calls/call.py
+++ calls/call.py
@@ -1,26 +1,36 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
-from bs4 import BeautifulSoup
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
+# Pull these parameters from the webapp
+id = '###'
+password = '###'
+
+# Call variables
+caller = '+18507792976'
+zoom_number = '+16468769923'
+
 
 # make sure to add environment variables for SID and TOKEN
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
-with open('instructions.xml') as f:
-    instructions = f.read()
+url = 'https://handler.twilio.com/twiml/EHd4aeeecb3b167c1c7cb49b755d9dbb27'
 
-print(instructions)
+url = url + '?id=' + str(id) + '&password=' + str(password)
+d = 'ww' + id + '#wwww*' + password + '#'
 
 call = client.calls.create(
-                        # url='http://demo.twilio.com/docs/voice.xml',
-                        twiml = '<Response><Gather input="speech" timeout="3"></Gather><Say>Ahoy there!</Say></Response>',
-                        to='+16174172510', #16177510502
-                        from_='+18507792976'
+    send_digits=d,
+
+                        twiml="<Response> <Record/> <Say voice='Polly.Amy'> Thanos did nothing "
+                              "wrong! </Say> </Response>",
+                        to=zoom_number,
+                        from_=caller
                     )
+
+
+
 
 print(call.sid)
