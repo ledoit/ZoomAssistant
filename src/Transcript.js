416f37fc70196ee1ac62b9f484d761e8f03b140f
specificlanguage <specificlanguage@gmail.com>
2021-11-14 00:06:13 -0500

    React frontend form changes
    Changed some javascript fields to make it clearer
    - Transcript should be updated occasionally for every call?
    - Different fields for password & meeting id, which weren't present before

--- src/Transcript.js
+++ src/Transcript.js
@@ -1,13 +1,17 @@
 import { Button, Form, FormGroup, FormControl} from "react-bootstrap";
 import React from 'react';
+import Invite from "./Invite";
 
 class Transcript extends React.Component {
 
     constructor(props) {
         super(props)
 
+        // You may want to think of adding a way to start transcribing once the site is connected to a call
+
         this.state = {
-            taskValue: ''
+            transcript_value: "",
+            zoom_call: <Invite/>
         }
     }
 
@@ -16,32 +20,29 @@ class Transcript extends React.Component {
         this.props.updatedTaskList(this.state.taskValue);
 
         this.setState({
-            taskValue: ''
+            transcript_value: ""
         })
     }
 
     handleChange = (event) => {
         this.setState({
-            taskValue: event.target.value
+            transcript_value: ""
         })
     }
 
-    removeCompletedTasks = () => {
-        this.props.handleCompletedTasks();  
-    }
-
     render() {
-        const formInstance = (
-            <Form inline onSubmit={this.handleSubmit}>
-                <FormGroup controlId="formInlineName">
-                    <textarea class="form-control" placeholder="Transcript starts here" rows="8" value={this.state.taskValue} onChange={this.handleChange} />
-                </FormGroup>
-            </Form>
-        );
+        // How do we constantly get new data from the Twilio MediaStream?
+        // while (this.state.zoom_call.state.meeting != ""){
+            const trans = (
+                <p class="text-break">
+                    {this.state.transcript_value}
+                </p>
+            );
+        // }
 
         return (
-            <div className="taskInputs">
-                {formInstance}
+            <div className="transcript">
+                {trans}
             </div>
         );
     }
